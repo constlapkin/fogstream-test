0021ca121d899b902e74f1749eeb1421475fb01e
Constantine Lapkin <constlapkin@gmail.com>
2019-11-18 16:17:43 +1000

    began to do form validation, status of letters, sending messages

--- mail/admin.py
+++ mail/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Mail
 
 
-admin.site.register(Mail)
+class MailAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status')
+
+
+admin.site.register(Mail, MailAdmin)
--- mail/views.py
+++ mail/views.py
@@ -10,22 +10,19 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 
-@method_decorator(login_required, name='dispatch')
-class MailListView(CreateView):
-    form_class = MailForm
-    template_name = 'mail/create_mail.html'
+class AjaxableResponseMixin(object):
+    def form_invalid(self, form):
+        response = super(AjaxableResponseMixin, self).form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        else:
+            return response
 
     def form_valid(self, form):
-        return super().form_valid(form)
-
-    def get(self, request):
-        return super().get(request)
-
-    def post(self, request):
-        if request.is_ajax():
-            email = request.POST.get('email', '')
-            text = request.POST.get('text', '')
-            title = request.POST.get('title', '')
+        if self.request.is_ajax():
+            email = self.request.POST.get('email', None)
+            text = self.request.POST.get('text', None)
+            title = self.request.POST.get('title', None)
             json_url = urlopen('http://jsonplaceholder.typicode.com/users')
             data = json.loads(json_url.read())
             new_data = {}
@@ -43,14 +40,30 @@ class MailListView(CreateView):
                 text = text + str(str_data)
             else:
                 text = text + email
+
+            status = send_mail(title,
+                               text,
+                               'fogs-test@yandex.ru',
+                               ['fogs-test@yandex.ru'])
+            if status:
+                new_data['status'] = True
+            else:
+                new_data['status'] = False
+
             if email and text and title:
-                mail_m = Mail(title=title, email=email, text=text)
+                mail_m = Mail(author=self.request.user,
+                              title=title,
+                              email=email,
+                              text=text,
+                              status=new_data['status'])
                 mail_m.save()
 
-            send_mail(title,
-                      text,
-                      'fogs-test@yandex.ru',
-                      ['fogs-test@yandex.ru'])
             return JsonResponse(new_data)
         else:
             return HttpResponse(False)
+
+
+@method_decorator(login_required, name='dispatch')
+class MailListView(AjaxableResponseMixin, CreateView):
+    form_class = MailForm
+    template_name = 'mail/create_mail.html'
